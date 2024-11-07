bbc202703fc0b9cda2889da21fae49c543454e38
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-07 17:00:36 +0100

    commit Tarea 4.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,18 @@ export class AppComponent {
   public title:string = 'Mi primera App de Angular';
   public subtitle:string = 'Esto va a ser duroh.';
   public contador:number = 10;
+  public x:number = 2;
 
   incrementarPor():void {
-    this.contador+=1
+    this.contador+=this.x
   }
 
   eliminarPor():void {
-    this.contador-=1
+    this.contador-=this.x
+  }
+
+  resetear():void {
+    this.contador=0
   }
 
 }
