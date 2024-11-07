2eb1d80b33c7ff8b719d2ff9a5f27d73c76f43c5
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-07 16:29:48 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-bases';
+  title = 'Mi primera App de Angular';
+  subtitle = 'Esto va a ser duroh.';
+  contador = 10;
 }
