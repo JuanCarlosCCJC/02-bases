08d895f8022152cca823813a564cff4638d835e2
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-18 21:25:41 +0100

    ngtemplate

--- src/app/heroes/lista/lista.component.ts
+++ src/app/heroes/lista/lista.component.ts
@@ -11,6 +11,13 @@ import { CommonModule } from '@angular/common';
 export class ListaComponent {
 
   // Creamos el array para poder leerlo
-  public nombresHeroes:string[] = ['All Might','Bakugo','Midoriya']
+  public nombresHeroes:string[] = ['All Might','Bakugo','Midoriya'];
+  // Creamos la avriable heroe eliminado para ng-template
+  public heroeEliminado?: string;
+
+  // Metodo que elimina y guardo a quien elimina
+  public eliminaUltimoHeroe():void {
+    this.heroeEliminado = this.nombresHeroes.pop()
+  }
 
 }
