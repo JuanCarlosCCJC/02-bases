90def931cba73d5b98c6f5bf3e07c9b39ffec7a1
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-19 11:40:34 +0100

    Terminado 4.3

--- src/app/Instituto/instituto2/instituto2.component.ts
+++ src/app/Instituto/instituto2/instituto2.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Interfaz para el colegio
+interface Colegio {
+  nombre: string;
+  localidad: string;
+  familiasProfesionales: string[];
+  logo: string;
+}
+
 @Component({
   selector: 'app-Instituto-instituto2',
   standalone: true,
@@ -8,9 +16,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './instituto2.component.html',
   styleUrl: './instituto2.component.css'
 })
+
+
 export class Instituto2Component {
 
-  public colegios = [
+  public colegios: Colegio[] = [
 
     {
       nombre: 'IES Playamar',
@@ -39,6 +49,9 @@ export class Instituto2Component {
 
   public contador = 0;
 
+  public pila:Colegio[]=[];
+
+  //Funciones
   public anterior(): void {
 
     if (this.contador > 0){
@@ -52,6 +65,42 @@ export class Instituto2Component {
     // Decimos menos 1 por que el length devuelve 3 pero en el array es 2 el final.
     if (this.contador < this.colegios.length - 1) {
       this.contador++;
+    }
+
+  }
+
+  public eliminar():void{
+
+    // Lo que hacemos es comprobar si el array tiene algun elemento
+    if (this.colegios.length > 0) {
+
+      // Splice elimina y guarda el de la posicion actual
+      const centroEliminado = this.colegios.splice(this.contador, 1)[0];
+
+      this.pila.push(centroEliminado); //! Se agrega a la pila , para hacer esto he tenido que crear la interfaz colegio sino me daba error aqui.
+
+      // Realizo esto para ajustar el contador, ya que si elimino y el contador es mayor al maximo no puedo ejecutar bien la apalicacionn luego.
+      if (this.contador >= this.colegios.length) {
+        this.contador--;
+      }
+
+    }
+
+  }
+
+  public insertar():void{
+
+    // Primero comprobamos que la pila tenga algun elemento
+    if (this.pila.length > 0){
+
+      // Aqui recogemos el ultimo elemento de la pila
+      const centroInsertado = this.pila.pop()
+
+      //!Splice me pide que sea de tipo Colegio si o si,por lo que tengo que verificarlo, sino me sale error y no me deja ejecutar el codigo
+      if (centroInsertado !== undefined) {
+        this.colegios.splice(this.contador, 0, centroInsertado);  //1 valor es la posicion, 2 el numero que vamos a eliminar, en este caso 0, 3 el elemento que vamos a añadir
+      }
+
     }
 
   }
