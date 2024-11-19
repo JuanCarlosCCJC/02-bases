4511f29e5e2bc1e89cc4c0a83f31a6e80961ca74
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-19 11:05:38 +0100

    Medio realizado el ejercicio 4.3

--- src/app/Instituto/instituto2/instituto2.component.ts
+++ src/app/Instituto/instituto2/instituto2.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-Instituto-instituto2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './instituto2.component.html',
   styleUrl: './instituto2.component.css'
 })
 export class Instituto2Component {
+
+  public colegios = [
+
+    {
+      nombre: 'IES Playamar',
+      localidad: 'Torremolinos',
+      familiasProfesionales: ['Informática y Comunicaciones', 'Transporte', 'Mantenimiento'],
+      logo: 'logo-playamar.png'
+    },
+    {
+      nombre: 'IES Jacaranda',
+      localidad: 'Churriana',
+      familiasProfesionales: ['Hostelería y Turismo'],
+      logo: 'logo-jacaranda.jpeg'
+    },{
+      nombre: 'Jesús Marín',
+      localidad: 'Málaga',
+      familiasProfesionales: ['Hostelería y Turismo'],
+      logo: 'logo-jacaranda.jpeg'
+    },{
+      nombre: 'El divino pastor',
+      localidad: 'Málaga',
+      familiasProfesionales: ['Hostelería y Turismo'],
+      logo: 'logo-jacaranda.jpeg'
+    }
+
+  ];
+
+  public contador = 0;
+
+  public anterior(): void {
+
+    if (this.contador > 0){
+      this.contador--;
+    }
+
+  }
+
+  public siguiente(): void {
+
+    // Decimos menos 1 por que el length devuelve 3 pero en el array es 2 el final.
+    if (this.contador < this.colegios.length - 1) {
+      this.contador++;
+    }
+
+  }
+
 
 }
