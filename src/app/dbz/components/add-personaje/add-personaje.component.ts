1f9ca1e5440fe70758061cff7b3eca1c889ae65e
JuanCarlosCCJC <carmonacavello10@gmail.com>
2024-11-28 17:50:03 +0100

    Terminado el M5.6

--- src/app/dbz/components/add-personaje/add-personaje.component.ts
+++ src/app/dbz/components/add-personaje/add-personaje.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../../interfaces/personaje.interface';
 
 @Component({
   selector: 'dbz-add-personaje',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './add-personaje.component.css'
 })
 export class AddPersonajeComponent {
+
+  public personaje : Personaje = {
+    nombre:'',
+    fuerza: 0
+  }
+
+  public addPersonaje(): void {
+
+    console.log(this.personaje);
+
+  }
 
 }
